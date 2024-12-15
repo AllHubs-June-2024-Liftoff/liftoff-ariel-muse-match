6b1fb690d32b54b2d20c4a2ebd223a48060812fa
thickman10 <thickman10@gmail.com>
2024-12-15 16:08:06 -0600

    Continuing work

--- backend/src/main/java/com/gw/backend/service/StatsService.java
+++ backend/src/main/java/com/gw/backend/service/StatsService.java
@@ -1,5 +1,6 @@
 package com.gw.backend.service;
 
+import com.gw.backend.models.user.UserPreferences;
 import com.gw.backend.repository.UserPreferencesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,17 @@ public class StatsService {
 	@Autowired
 	UserPreferencesRepository userPreferencesRepository;
 
-	public Byte findPercentage(Long userId, Object searchTerm){
-		if(searchTerm == "artMovement"){
-			userPreferencesRepository.countArtMovementByUserId()
+	public Byte findPercentage(Long userId, Object filter) {
+		Long likes;
+		Long dislikes;
+        if (filter == "artMovement") {
+			likes = userPreferencesRepository.countArtMovementByUserId(userId, (String) filter, UserPreferences.Preference.LIKE);
+			dislikes = userPreferencesRepository.countArtMovementByUserId(userId, (String) filter, UserPreferences.Preference.DISLIKE);
 		}
-	}
+		if(filter == "artYearFinished"){
+			likes = userPreferencesRepository.countArtYearFinishedByUserId(userId, (String))
+		}
+    }
 
 	public Integer findTotal(Long userId, String searchTerm){
 
