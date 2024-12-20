38f9f823170796b43f4332b23a260bd1ddc4e3b3
treyhickman10 <thickman10@gmail.com>
2024-12-20 14:31:02 -0600

    Added code to frontend to handle changing API endpoints

--- frontend/src/pages/profile/Stats.jsx
+++ frontend/src/pages/profile/Stats.jsx
@@ -3,9 +3,10 @@ import React, { useEffect, useState } from "react";
 function Stats() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [uri, setUri] = useState("general");
 
   useEffect(() => {
-    fetch("http://localhost:8080/profile/stats/test")
+    fetch(`http://localhost:8080/profile/stats/${uri}`)
       .then((response) => response.json())
       .then((data) => {
         setData(data);
@@ -15,19 +16,25 @@ function Stats() {
         console.error("Error Fetching Data: ", error);
         setLoading(false);
       });
-  }, []);
+  }, [uri]);
 
   if (loading) {
     return <p>Loading...</p>;
   }
 
-  return (
-    <div>
-      <p>
-        {Object.keys(data)}: {data.Minimalist.total}
-      </p>
-    </div>
-  );
+  data.map((datum) => {
+    const liked = data[datum].likes;
+    const total = data[datum].total;
+    const percent = data[datum].percent;
+    return (
+      <div>
+        <h3>{datum}</h3>
+        <p>
+          Liked: {percent}% {`(${liked}/${total})`}
+        </p>
+      </div>
+    );
+  });
 }
 
 export default Stats;
