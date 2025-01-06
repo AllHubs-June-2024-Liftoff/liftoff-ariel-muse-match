c8c45cfbf3e762dcdb6ba6e6c2ac1de83b1faa2e
d-esda <onedesda@gmail.com>
2025-01-06 17:19:21 -0500

    project skeleton

---
+++ frontend/src/components/pages/UserProfile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Form from 'react-bootstrap/Form';
+import { useState, useEffect } from "react";
+
+export default function ViewProfile(params) {
+
+  const [userName, setUserName] = useState("");
+  const [myEmail, setMyEmail] = useState("");
+
+  useEffect(()=>{
+    setUserName("John Doe");
+    setMyEmail("john@placeholder.com")
+  }, []);
+
+  
+  return (
+    <>
+      <h4>@{userName}</h4>
+      <h5>Email: {myEmail}</h5>
+    </>
+  );
+}
