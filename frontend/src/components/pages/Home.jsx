3e5542f8ae8830f18776bf32030a8403e706e8f0
d-esda <onedesda@gmail.com>
2025-01-09 16:39:10 -0500

    coditionally shows muse cards on authed user

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -4,6 +4,7 @@ import Login from "./Login";
 import { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import LoadArtworks from '../DisplayArtworks'
+import { useAuth } from "../auth/AuthContext";
 
 
 
@@ -11,6 +12,7 @@ export default function Home(params) {
 
   const [loggedIn, setLoggedIn] = useState(false);
   const [data, setData] = useState('');
+  const {isAuthenticated} = useAuth()
 
   useEffect(() => {
     fetch('http://localhost:8080/api/hello')
@@ -26,9 +28,21 @@ export default function Home(params) {
 
   return (
     <div className="container mt-5">
-      <LoadArtworks />
-      {/* <h1>{data}</h1>
-      <Image className="w-25 mt-5 mb-5" style={{borderRadius: "6px"}} src="muse.png"/>
+
+      {!isAuthenticated ? 
+        <>
+          <h1>Welcome to MuseMatch</h1>
+          <Image className="w-25 mt-5 mb-5" style={{borderRadius: "6px"}} src="muse.png"/>
+          <p>MuseMatch is an art exploration app where you match with artists by swiping right on their art pieces.</p>
+          <Button href="/login"> Login</Button>  <Button href="sign-up"> Signup</Button>
+        </>
+      : 
+        <>
+          <LoadArtworks />
+        </>
+      }
+      {/* <h1>{data}</h1> */}
+      {/* <Image className="w-25 mt-5 mb-5" style={{borderRadius: "6px"}} src="muse.png"/>
       <p>MuseMatch is an art exploration app where you match with artists by swiping right on their art pieces.</p>
       {!loggedIn ? <><Button href="/login"> Login</Button>  <Button href="sign-up"> Signup</Button></>: <></> } */}
     </div>
