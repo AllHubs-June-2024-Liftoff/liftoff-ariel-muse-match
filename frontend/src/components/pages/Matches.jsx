074fa608535f9f27eba001cac43f4904835a2753
zebjafolla <113193465+zebjafolla@users.noreply.github.com>
2025-01-20 12:38:11 -0500

    trying to merge jake/dontae/zeb

---
+++ frontend/src/components/pages/Matches.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import CardList from './CardList';
+
+export default function Match() {
+  return (
+    <>
+      <h1>Matches</h1>
+      <CardList />
+    </>
+  );
+}
