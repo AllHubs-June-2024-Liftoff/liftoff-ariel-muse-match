1f829f7fa9d15d33850708cbb394c262436ca58e
treyhickman10 <thickman10@gmail.com>
2025-01-31 07:56:50 -0600

    Add Reflections model with JPA annotations for database mapping

---
+++ backend/src/main/java/com/gw/backend/models/Reflections.java
@@ -0,0 +1,12 @@
+package com.gw.backend.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "reflections")
+public class Reflections {
+
+
+
+}
